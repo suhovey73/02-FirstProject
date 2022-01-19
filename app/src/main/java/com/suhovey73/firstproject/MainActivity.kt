85528cdedfbd1393fb31ad3b1b781bf68e095fba
suhovey73 <suhovey73@gmail.com>
2022-01-19 20:24:17 +0700

    First commit

---
+++ app/src/main/java/com/suhovey73/firstproject/MainActivity.kt
@@ -0,0 +1,10 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.suhovey73.firstproject.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
