c70d78dada472bd002461813a6220114e67f52c6
suhovey73 <suhovey73@gmail.com>
2022-01-19 20:35:36 +0700

    Second commit

--- app/src/main/java/com/suhovey73/firstproject/MainActivity.kt
+++ app/src/main/java/com/suhovey73/firstproject/MainActivity.kt
@@ -1,6 +1,7 @@
+package com.suhovey73.firstproject
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.suhovey73.firstproject.R
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
